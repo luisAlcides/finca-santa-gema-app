617d48018c02f521f8c2c4d35b8cb98356f10952
luisAlcides <luisalcidesblandon@gmail.com>
2024-07-04 16:28:42 -0600

    static

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -125,8 +125,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
